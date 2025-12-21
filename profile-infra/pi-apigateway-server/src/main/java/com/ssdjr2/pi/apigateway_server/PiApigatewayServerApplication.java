cbb53ca48e46e9a5feea4351aef0713738d466a1
Jacinto R2 <balbino.v12@gmail.com>
2025-12-21 16:04:34 +0100

    only initializer apigateway server

---
+++ profile-infra/pi-apigateway-server/src/main/java/com/ssdjr2/pi/apigateway_server/PiApigatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.ssdjr2.pi.apigateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiApigatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiApigatewayServerApplication.class, args);
+	}
+
+}
---
+++ profile-infra/pi-apigateway-server/src/test/java/com/ssdjr2/pi/apigateway_server/PiApigatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssdjr2.pi.apigateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiApigatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
